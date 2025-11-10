8880e51126697b38d21ec1f829a62357c7ad4af1
A-Rahiym <a.raheeem0001@gmail.com>
2025-11-10 23:18:30 +0100

    latest commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,20 +34,12 @@ export const metadata: Metadata = {
   title: "PathPilot - Navigate Life with Confidence",
   description:
     "Empowering navigation for the visually impaired with AI-powered intelligence",
-  generator: "v0.app",
+  generator: " ",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/Logo.png",
+        type: "png",
       },
     ],
     apple: "/apple-icon.png",
